962f7e07ff7480d0dbce1533dcc8d931e344f5cb
ashish-t-2610 <190104017@hbtu.ac.in>
2022-08-08 21:07:58 +0530

    final-03

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className>
+      <header>
+        <a href="/">final</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
